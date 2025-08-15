1a048099aeb01621194730f86e54010877e9c8dc
SHAHIL <110395532+Shahilali5@users.noreply.github.com>
2025-08-15 22:34:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ import os
 class Config:
     API_ID = "29727048"
     API_HASH = "38d104adbd94c66a349714abd7977d80"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
-    TOKEN = os.environ.get("TOKEN", None)
+    TOKEN = "7378446791:AAEXhP9KzrPNHZkN0gIgqklw7Vm3CFebgOU"
     MONGO_URL = "mongodb+srv://Chatgpt:Chatgpt%40123@chatgpt.56l90.mongodb.net/?retryWrites=true&w=majority&appName=Chatgpt"
     START_PIC = "https://envs.sh/aKf.mp4"
-    SUDOERS = filters.user(["8129810243"])
+    SUDOERS = filters.user(["8129810243", "6409133123"])
